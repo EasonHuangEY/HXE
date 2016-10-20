1b19b441400821c906b63cace42ee95dd741aa50
SAP HANA Academy <hanaacademy@sap.com>
2016-10-20 16:36:05 +0200

    Managing Encryption Keys

--- SSFS/changeSSFSMasterKeys.sh
+++ SSFS/changeSSFSMasterKeys.sh
@@ -2,90 +2,80 @@
 
 ## SAP HANA Academy
 ## Playlist: SAP HANA express edition
-## Tutorial video: https://www.youtube.com/watch?v=F53iPBsq6AY
+## Tutorial video:
 ## Sample script provided for educational purposes
 ## Info: hanaacademy@sap.com
 
 echo '********************************************************************************************'
 echo 'Script to change the SSFS master keys (instance and PKI) and generate new DPAPI root key'
 echo 'Before running this script, read'
-echo ' - SAP Note 2183624 and 2229831'
-echo ' - SAP HANA Administration Guide, section Managing Encryption Keys'
+echo ' - SAP Note 2183624, 2228829 and 2229831'
+echo ' - SAP HANA Administration Guide, Managing Encryption Keys'
 echo ''
-echo 'Make sure that the internal credential is empty before running this script'
-echo ' SELECT * FROM SYS.CREDENTIALS / SYS.P_DPAPI_KEY_ / PSE_CERTIFICATES'
+echo 'During the procedure the SAP HANA database is stopped and restarted'
 echo '********************************************************************************************'
 echo ''
 read -rsp $'Use ctrl-c to exit or press enter to continue...\n'
 
-echo .
-echo 'Stopping the SAP HANA database ...'
-# Command (script) 'HDB stop' calls command sapcontrol with preconfigured parameters
-# Using HDB stop has the advantage that the prompt is returned when the database is stopped.
-# For distributed systems, sapcontrol is required
-## sapcontrol -nr 00 -function Stop
-HDB stop
-echo .
-HDB info
-echo .
-echo 'Verify that SAP HANA is stopped (no hdbnameserver process)'
-echo 'Otherwise exit this script and first stop SAP HANA (HDB stop).'
-echo .
-read -rsp $'Use ctrl-c to exit or press enter to continue...\n'
+# Update the SSFS master key for PKI
+# Path = $DIR_GLOBAL/security/rsecssfs/data and ../key
 
-echo .
-echo 'changing PKI SSFS master key (securing internal communication)'
+echo ''
+echo '>>> changing PKI SSFS master key (securing internal communication)'
+echo ''
 export RSEC_SSFS_DATAPATH=/usr/sap/HXE/SYS/global/security/rsecssfs/data
 export RSEC_SSFS_KEYPATH=/usr/sap/HXE/SYS/global/security/rsecssfs/key
 rsecssfx changekey $(rsecssfx generatekey -getPlainValueToConsole)
-echo .
+echo ''
 rsecssfx list
-sleep 5
 
-echo .
-echo 'changing instance SSFS master key (securing the internal encryption service)'
+# Update the SSFS master key for the internal encryption service
+# Path = $DIR_GLOBAL/hdb/security/ssfs
+
+echo ''
+echo '>>> changing instance SSFS master key (securing the internal encryption service)'
+echo ''
 export RSEC_SSFS_DATAPATH=/usr/sap/HXE/SYS/global/hdb/security/ssfs
 export RSEC_SSFS_KEYPATH=/usr/sap/HXE/SYS/global/hdb/security/ssfs
 rsecssfx changekey $(rsecssfx generatekey -getPlainValueToConsole)
-echo .
+echo ''
 rsecssfx list
-sleep 5
 
-echo .
-echo 'generating a new root key for the secure internal credential store (DPAPI)'
+# Administration Guide and Note 2183624 document to set the ssfs_key_file_path parameter
+# This is only require if the default path of the key file is changed:
+# $DIR_GLOBAL/hdb/security/ssfs
+# Note that you cannot change the location of the secure store (datapath)
+# hdbsql -u system -d SystemDB "ALTER SYSTEM ALTER CONFIGURATION ('global.ini', 'SYSTEM') SET ('cryptography', 'ssfs_key_file_path') = '/usr/sap/HXE/SYS/global/hdb/security/ssfs' WITH RECONFIGURE"
+
+# Shutdown the database enable DPAPI root key generation
+echo '>>> Stopping the SAP HANA database'
+echo ''
+HDB stop
+echo ''
+echo '>>> generating a new root key for the secure internal credential store (DPAPI)'
+echo ''
 hdbnsutil -generateRootKeys --type=DPAPI
-
-echo .
-echo 'Starting the SAP HANA database ...'
-# Command (script) 'HDB start' calls command sapcontrol with preconfigured parameters
-# Using HDB start has the advantage that the prompt is returned when the database is started.
-# For distributed systems, sapcontrol is required
-## sapcontrol -nr 00 -function Start
+echo ''
+echo '>>> Starting the SAP HANA database'
 HDB start
-echo .
-echo 'Verify that SAP HANA is started (hdbnameserver process)'
-echo 'Otherwise exit this script and first start SAP HANA (HDB start).'
-echo .
-HDB info
-echo .
-read -rsp $'Use ctrl-c to exit or press enter to continue...\n'
-
-# The default path of the key file is $DIR_GLOBAL/hdb/security/ssfs.
-# If you change the default path, you may need to reconfigure it in the event of a system rename.
-# SQL command just sets the path to the default path, making it visible (not required),
-echo .
-echo 'Enter the SystemDB SYSTEM user password to add the SSFS key file path'
-hdbsql -u system -d SystemDB "ALTER SYSTEM ALTER CONFIGURATION ('global.ini', 'SYSTEM') SET ('cryptography', 'ssfs_key_file_path') = '/usr/sap/HXE/SYS/global/hdb/security/ssfs' WITH RECONFIGURE"
 
 # cf. SAP Note 2228829 - How to Change the DPAPI Root Key
-echo .
-echo 'Resetting the consistency information in the SSFS'
+echo ''
+echo '>>> resetting the consistency information in the SSFS'
+echo ''
 hdbcons "crypto ssfs resetConsistency" -e hdbnameserver
-sleep 3
+echo ''
 hdbcons "crypto ssfs resetConsistency" -e hdbnameserver
-echo .
-echo 'Enter the SystemDB SYSTEM user password to set the (default) path to the instance SSFS master key'
+echo ''
+echo '>>> Enter the SystemDB SYSTEM user password to create the new DPAPI key'
 hdbsql -u system -d SystemDB "ALTER SYSTEM APPLICATION ENCRYPTION CREATE NEW KEY"
+
+echo ''
+
+echo '********************************************************************************************'
+echo ' Backup up your HANA system after changing the encryption key'
+echo '********************************************************************************************'
+echo ''
 
 # optional check
 ##echo .
